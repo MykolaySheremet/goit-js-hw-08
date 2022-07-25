34129b0ce27e66156c0e86ba59831f5b98e38ae0
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-07-25 19:40:34 +0300

    do task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'   
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const keyForStorage = "videoplayer-current-time";
+
+function timeFollow(time) {
+    const saveTimeString = JSON.stringify(time)
+    localStorage.setItem(keyForStorage, saveTimeString)
+};
+
+const saveTimeString = localStorage.getItem(keyForStorage);
+
+const parsSaveTimeArray = JSON.parse(saveTimeString);
+
+player.on("timeupdate", throttle(timeFollow, 1000))
+
+player.setCurrentTime(parsSaveTimeArray.seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
