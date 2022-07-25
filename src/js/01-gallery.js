2b60423cbc9b9242be8669554581b23669217a60
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-07-25 14:45:02 +0300

    do task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,22 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+const listOfGallery = document.querySelector('.gallery');
+
+
+const renderItemsGallery = (pictures) => galleryItems.map(({ original, preview, description }) =>
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`).join("");
+
+listOfGallery.insertAdjacentHTML('afterbegin', renderItemsGallery(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    })
+console.log(lightbox);
